c8be29c6c2a73500e37ab9f4a51ebfa2d457179c
Kenneth Lyerly <kenput3r@gmail.com>
2021-05-12 14:13:04 -0700

    add contact form

---
+++ src/components/private-events.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from "styled-components"
+
+const PrivateEvents = () => {
+  return (
+    <Component>
+      <div>Private Events</div>
+    </Component>
+  )
+}
+
+export default PrivateEvents
+
+const Component = styled.div``
